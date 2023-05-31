c2d26978370aa19c2e26f1001f272fc7180d6131
AhmedEnnaime <97989224+AhmedEnnaime@users.noreply.github.com>
2023-05-31 14:46:45 +0100

    refactor: :zap: small touch

--- backend/src/main/java/com/spring/crud/api/backend/controller/TodoController.java
+++ backend/src/main/java/com/spring/crud/api/backend/controller/TodoController.java
@@ -2,7 +2,6 @@ package com.spring.crud.api.backend.controller;
 
 import com.spring.crud.api.backend.model.Todo;
 import com.spring.crud.api.backend.service.TodoService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
